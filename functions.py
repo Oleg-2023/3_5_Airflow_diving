d564d6a2153fe8456e3de049656140675b0473e9
Oleg-2023 <144448179+Oleg-2023@users.noreply.github.com>
2023-11-20 17:43:23 +0500

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,40 @@
+import requests
+from datetime import datetime
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import Variable
+
+conn_id = Variable.get("conn_name")
+SOURCE = Variable.get("CURRENCY_SOURCE")
+CURRENCY = Variable.get("BASE_CURRENCY")
+ACCESS_KEY = Variable.get("API_ACCESS_KEY")
+
+SOURCE = 'BTC'
+CURRENCY = 'RUB'
+
+def get_url():
+    # Запрос к Web API
+    url = 'http://api.exchangerate.host/live?access_key={}&'\
+          'source={}&currencies={}'.format(ACCESS_KEY, SOURCE, CURRENCY)
+    return requests.get(url)
+
+def add_data_psql():
+    hook = PostgresHook(postgres_conn_id=conn_id)
+    conn = hook.get_conn()
+    cursor = conn.cursor()
+    try:
+        response = get_url()
+        data = response.json()
+        get_date = datetime.utcfromtimestamp(data["timestamp"]).strftime('%Y-%m-%d %H:%M:%S')
+        rate = data["quotes"][SOURCE+CURRENCY]
+        sql = f'''
+                INSERT INTO quotes (get_date, base_rate, target_rate, rate)
+                VALUES ('{get_date}', '{CURRENCY}', '{SOURCE}', '{rate}');
+        '''
+        cursor.execute(sql)
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+    except Exception as error:
+        conn.rollback()
+        raise Exception(f'Ошибка добавления данных: {error}')
